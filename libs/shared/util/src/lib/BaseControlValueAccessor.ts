69d3dc13a0733889d1c01af1610c57e6bc5bd0ce
Samuel <samuelvvelzen@icloud.com>
2025-06-02 16:47:32 +0200

    create basecontrolvalue accessor

---
+++ libs/shared/util/src/lib/BaseControlValueAccessor.ts
@@ -0,0 +1,86 @@
+import {
+  Directive,
+  ElementRef,
+  Optional,
+  output,
+  QueryList,
+  Self,
+  ViewChildren,
+} from '@angular/core';
+import { ControlValueAccessor, NgControl, NgModel } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { shareReplay, skip } from 'rxjs/operators';
+
+@Directive()
+export class BaseValueAccessor<T> implements ControlValueAccessor {
+  @ViewChildren(NgModel) models!: QueryList<NgModel>;
+  @ViewChildren(NgModel, { read: ElementRef }) modelEls!: QueryList<ElementRef>;
+
+  private _value$ = new BehaviorSubject<T | undefined | null>(undefined);
+  private _isFirstTime = true;
+
+  disabled = false;
+  valueChanges$ = this._value$.pipe(skip(1), shareReplay(1));
+
+  readonly selected = output<T | null>();
+
+  private _onChange: (val: T | undefined | null) => void = () => void 0;
+  private _onTouched: () => void = () => void 0;
+
+  get value(): T | undefined | null {
+    return this._value$.value;
+  }
+
+  set value(val: T | undefined | null) {
+    if (this._value$.value === val) return;
+    this._value$.next(val);
+    this._onChange(val);
+    this._onTouched();
+    this.selected.emit(val || null);
+  }
+
+  constructor(@Optional() @Self() private _control?: NgControl) {
+    this._setValueAccessor(_control);
+  }
+
+  writeValue(val?: T): void {
+    if (this._skipWrite(val)) return;
+    this._value$.next(val);
+  }
+
+  registerOnChange(fn: (val?: T | null) => void): void {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this._onTouched = fn;
+  }
+
+  setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
+
+  triggerTouch(): void {
+    this._onTouched();
+  }
+
+  private _setValueAccessor(control?: NgControl): void {
+    if (control) {
+      control.valueAccessor = this;
+    }
+  }
+
+  /**
+   * NgModel has a long-running bug, where the first value
+   * is null. If this is the case, we skip the first value, so the
+   * value will always be T or undefined
+   *
+   * The Angular Issue: https://github.com/angular/angular/issues/14988
+   */
+  private _skipWrite(val: T | undefined): boolean {
+    if (!this._isFirstTime) return false;
+    this._isFirstTime = false;
+
+    return val === null && this._control instanceof NgModel;
+  }
+}
