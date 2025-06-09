7559377d0e18a8a8a161b9b4bc8794923da61175
Samuel <samuelvvelzen@icloud.com>
2025-06-09 23:04:37 +0200

    implement calculator

--- apps/MyWheelsCalculator/src/app/app.component.ts
+++ apps/MyWheelsCalculator/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CalculatorComponent } from '@mwc/calculator';
 
 @Component({
-  imports: [RouterModule],
+  imports: [CalculatorComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'MyWheelsCalculator';
-}
+export class AppComponent {}
