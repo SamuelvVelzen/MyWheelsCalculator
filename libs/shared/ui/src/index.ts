03e551bee01488d892685a778a3dfbea27b590d0
Samuel <samuelvvelzen@icloud.com>
2025-06-15 21:33:09 +0200

    create toggle button

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,4 +1,6 @@
 export * from './lib/forms/radio-buttons-card/radio-buttons-card.component';
 export * from './lib/forms/slider-number/slider-number.component';
 export * from './lib/forms/slider/slider.component';
+
+export * from './lib/button/toggle-button/toggle-button.component';
 export * from './lib/forms/fieldset/fieldset.component';
---
+++ libs/shared/ui/src/lib/button/toggle-button/toggle-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BaseFormInputs } from '../../_types/BaseFormInputs';
+
+@Component({
+  selector: 'mwc-toggle-button',
+  templateUrl: './toggle-button.component.html',
+  styleUrls: ['./toggle-button.component.css'],
+  standalone: true,
+  imports: [FormsModule],
+})
+export class ToggleButtonComponent extends BaseFormInputs<boolean> {
+  labelText = input.required<string>();
+}
