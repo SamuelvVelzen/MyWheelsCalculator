6a2b157bdf3475e6a70ef9a48e61dd8066178e28
Samuel <samuelvvelzen@icloud.com>
2025-06-18 21:38:10 +0200

    rename currencypipe

--- libs/shared/util/src/lib/pipes/currency.pipe.ts
+++ libs/shared/util/src/lib/pipes/currency.pipe.ts
@@ -1,21 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'currency',
+  name: 'mwcCurrency',
+  standalone: true,
 })
 export class CurrencyPipe implements PipeTransform {
-  private _currencyFormatter = new Intl.NumberFormat(navigator.language, {
-    style: 'currency',
-    currency: 'EUR',
-  });
-
-  transform(value: number | string): string {
+  transform(value: number | string, showDecimals = true): string {
     const val = Number(value);
 
     if (isNaN(val)) {
       return typeof value === 'string' ? value : value.toString();
     }
 
-    return this._currencyFormatter.format(val);
+    const formatter = new Intl.NumberFormat(navigator.language, {
+      style: 'currency',
+      currency: 'EUR',
+      minimumFractionDigits: showDecimals ? 2 : 0,
+      maximumFractionDigits: showDecimals ? 2 : 0,
+    });
+
+    return formatter.format(val);
   }
 }
