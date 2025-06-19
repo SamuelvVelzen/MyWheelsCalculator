13bc666d8b8059a15414c59e7f202d4243dc4a62
Samuel <samuelvvelzen@icloud.com>
2025-06-19 23:16:54 +0200

    improve spacing and add button on navbar

--- apps/MyWheelsCalculator/src/app/main-layout/main-layout.component.ts
+++ apps/MyWheelsCalculator/src/app/main-layout/main-layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from '@mwc/ui';
+import { CurrencyPipe } from '@mwc/util';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CurrencyPipe, ButtonComponent],
 })
 export class MainLayoutComponent {}
