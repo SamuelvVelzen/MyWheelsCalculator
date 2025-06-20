7f59296b13ec03b5370914c1860a0f26882a2a92
Samuel <samuelvvelzen@icloud.com>
2025-06-20 23:07:43 +0200

    improve layout and styling of price

--- libs/feature/calculator/src/lib/price/price.component.ts
+++ libs/feature/calculator/src/lib/price/price.component.ts
@@ -1,15 +1,17 @@
-import { Component, inject, signal } from '@angular/core';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ButtonComponent, ButtonTypeEnum, ThemeEnum } from '@mwc/ui';
 import { CurrencyPipe } from '@mwc/util';
 import { PriceService } from '../_services/price.service';
 import { AbonnementOptions } from '../_types/AbonnementOptionsEnum';
+import { TripOptions, TripOptionsEnum } from '../_types/TripOptionsEnum';
 
 @Component({
   selector: 'mwc-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css'],
   standalone: true,
-  imports: [CurrencyPipe, ButtonComponent],
+  imports: [CurrencyPipe, ButtonComponent, NgTemplateOutlet, CommonModule],
 })
 export class PriceComponent {
   priceService = inject(PriceService);
@@ -18,6 +20,7 @@ export class PriceComponent {
   basePrice = this.priceService.basePrice;
   hourPrice = this.priceService.hourPrice;
   kilometerPrice = this.priceService.kilometerPrice;
+  kilometers = this.priceService.kilometers;
   extraCosts = this.priceService.extraCosts;
   abonnement = this.priceService.abonnement;
 
@@ -26,4 +29,80 @@ export class PriceComponent {
   themeEnum = ThemeEnum;
 
   showPriceDetails = signal(false);
+
+  priceDetails = computed(() => {
+    return [
+      {
+        label: `Discount (${
+          this.abonnementOptions[this.abonnement()].discount
+        }%)`,
+        totalCost: this.totalPrice() - this.basePrice(),
+        hide: this.abonnementOptions[this.abonnement()].discount === 0,
+      },
+      {
+        label: 'Base price',
+        totalCost: this.basePrice(),
+        children: [
+          {
+            label: 'Hour price',
+            totalCost: this.hourPrice(),
+          },
+          {
+            label: 'Kilometer price',
+            totalCost: this.kilometerPrice(),
+            children: [
+              {
+                label: 'Total kilometers',
+                value: this.kilometers(),
+              },
+              {
+                label: `Trip price (${
+                  TripOptions[this.priceService.trip()].title
+                })`,
+                totalCost: TripOptions[this.priceService.trip()].price,
+                hide: this.priceService.trip() === TripOptionsEnum.None,
+              },
+              {
+                label: 'Extra km',
+                value: this.priceService.extraKm(),
+                hide: this.priceService.extraKm() === 0,
+              },
+            ],
+          },
+          {
+            label: 'Extra costs',
+            totalCost: this.extraCosts(),
+            children: [
+              {
+                label: 'Start price',
+                totalCost: PriceService.startPrice,
+                hide: !this.priceService.hasStartPrice(),
+              },
+              {
+                label: 'Deposit',
+                totalCost: PriceService.depositPrice,
+                hide: this.priceService.hasDepositPaid(),
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  });
 }
+
+export type IPriceDetail =
+  | {
+      label: string;
+      hide?: boolean;
+      children?: IPriceDetail[];
+    } & (
+      | {
+          totalCost?: never;
+          value: number;
+        }
+      | {
+          totalCost: number;
+          value?: never;
+        }
+    );
