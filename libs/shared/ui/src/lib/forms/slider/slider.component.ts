961602b5059845ec350bc9310f12b5f3b212f2ed
Samuel <samuelvvelzen@icloud.com>
2025-06-21 00:48:25 +0200

    implement custom range styling

--- libs/shared/ui/src/lib/forms/slider/slider.component.ts
+++ libs/shared/ui/src/lib/forms/slider/slider.component.ts
@@ -1,5 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {
+  Component,
+  effect,
+  ElementRef,
+  inject,
+  Injector,
+  OnInit,
+  viewChild,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BaseInputControls } from '../../_types/BaseInputControls';
 
@@ -10,4 +18,41 @@ import { BaseInputControls } from '../../_types/BaseInputControls';
   imports: [FormsModule, CommonModule],
   standalone: true,
 })
-export class SliderComponent extends BaseInputControls<number> {}
+export class SliderComponent
+  extends BaseInputControls<number>
+  implements OnInit
+{
+  private readonly _injection = inject(Injector);
+
+  rangeElement = viewChild.required<ElementRef<HTMLInputElement>>('range');
+
+  ngOnInit(): void {
+    effect(
+      () => {
+        const value = this.valueSignal();
+
+        if (value) {
+          this._setProgress(value);
+        }
+      },
+      { injector: this._injection }
+    );
+  }
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = Number(target.value);
+
+    this._setProgress(value);
+  }
+
+  private _setProgress(value: number) {
+    const progress =
+      (value / Number(this.rangeElement().nativeElement.max)) * 100;
+
+    this.rangeElement().nativeElement.style.setProperty(
+      '--progress',
+      `${progress}%`
+    );
+  }
+}
