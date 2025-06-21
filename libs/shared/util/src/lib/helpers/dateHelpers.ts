1fd3c963e0a7743535929e152fa4eef27e01d6cf
Samuel <samuelvvelzen@icloud.com>
2025-06-21 23:33:29 +0200

    improve datehelpers to accept date or string

--- libs/shared/util/src/lib/helpers/dateHelpers.ts
+++ libs/shared/util/src/lib/helpers/dateHelpers.ts
@@ -1,11 +1,27 @@
 import { isAfter, isBefore } from 'date-fns';
 
 export class DateHelpers {
-  static isAfter(date1: string, date2: string): boolean {
-    return isAfter(new Date(date1), new Date(date2));
+  static isAfter(date1: string | Date, date2: string | Date): boolean {
+    if (typeof date1 === 'string') {
+      date1 = new Date(date1);
+    }
+
+    if (typeof date2 === 'string') {
+      date2 = new Date(date2);
+    }
+
+    return isAfter(date1, date2);
   }
 
-  static isBefore(date1: string, date2: string): boolean {
-    return isBefore(new Date(date1), new Date(date2));
+  static isBefore(date1: string | Date, date2: string | Date): boolean {
+    if (typeof date1 === 'string') {
+      date1 = new Date(date1);
+    }
+
+    if (typeof date2 === 'string') {
+      date2 = new Date(date2);
+    }
+
+    return isBefore(date1, date2);
   }
 }
