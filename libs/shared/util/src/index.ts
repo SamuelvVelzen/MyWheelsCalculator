faec82dc8afb4aab47603ad37f6e1c4158faf9f5
Samuel <samuelvvelzen@icloud.com>
2025-06-21 15:41:42 +0200

    create injectiontoken for windows

--- libs/shared/util/src/index.ts
+++ libs/shared/util/src/index.ts
@@ -2,4 +2,7 @@ export * from './lib/BaseControlValueAccessor';
 export * from './lib/pipes/currency.pipe';
 
 export * from './lib/helpers/helpers';
+
+export * from './lib/injectiontokens/window';
+
 export * from './lib/services/query-params.service';
