4ef39159336da877dd220ca623e8b137607f1fcd
Samuel <samuelvvelzen@icloud.com>
2025-06-22 00:08:14 +0200

    update updating value

--- libs/shared/ui/src/lib/forms/input/input-datetime/input-datetime.component.ts
+++ libs/shared/ui/src/lib/forms/input/input-datetime/input-datetime.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, output, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DateHelpers } from '@mwc/util';
 import {
   FlatpickrDirective,
   provideFlatpickrDefaults,
 } from 'angularx-flatpickr';
 import { FlatPickrOutputOptions } from 'angularx-flatpickr/lib/flatpickr.directive';
-import { addHours, setMinutes } from 'date-fns';
 import { BaseDateInputs } from '../../../_types/BaseDateInputs';
 
 @Component({
@@ -27,25 +27,11 @@ export class InputDatetimeComponent extends BaseDateInputs<Date> {
   updateValue(event: FlatPickrOutputOptions): void {
     const date = event.selectedDates[0];
 
-    const roundedDate = this._roundToNearestStep(new Date(date));
+    const roundedDate = DateHelpers.roundToNearestStep(
+      date,
+      this.minuteIncrement()
+    );
 
     this.value = roundedDate;
-  }
-
-  private _roundToNearestStep(date: Date): Date {
-    const currentMinutes = date.getMinutes();
-
-    const remainder = currentMinutes % this.minuteIncrement();
-
-    const roundedMinutes =
-      currentMinutes + (this.minuteIncrement() - remainder);
-
-    // Handle overflow to next hour
-    if (roundedMinutes >= 60) {
-      date = addHours(date, 1);
-      return setMinutes(date, 0);
-    }
-
-    return setMinutes(date, roundedMinutes);
   }
 }
