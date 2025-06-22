fcf835f70bc1fb9e90e91cbcc77acaadbc0d3f5c
Samuel <samuelvvelzen@icloud.com>
2025-06-22 15:29:41 +0200

    improve language select component with styling and func

--- libs/shared/ui/src/lib/language-select/language-select.component.ts
+++ libs/shared/ui/src/lib/language-select/language-select.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import { LanguageEnum } from '@mwc/util';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { LanguageEnum, LanguageService } from '@mwc/util';
 
 @Component({
   selector: 'mwc-language-select',
   templateUrl: './language-select.component.html',
   styleUrls: ['./language-select.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class LanguageSelectComponent {
+  private languageService = inject(LanguageService);
+
   availableLanguages = [LanguageEnum.EN, LanguageEnum.NL];
 
+  // Get current language from the service
+  currentLanguage = this.languageService.currentLanguage;
+
   setLanguage(language: LanguageEnum) {
-    console.log(language);
+    this.languageService.setLanguage(language);
   }
 }
