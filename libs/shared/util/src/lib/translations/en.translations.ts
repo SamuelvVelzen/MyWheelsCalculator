89881b977062db7ee6f4d5941e057323a1442aa8
Samuel <samuelvvelzen@icloud.com>
2025-06-22 15:30:15 +0200

    add en and nl translations

---
+++ libs/shared/util/src/lib/translations/en.translations.ts
@@ -0,0 +1,24 @@
+export const enTranslations = {
+  // Calculator
+  'calculator.title': 'mywheels calculator',
+
+  'calculator.price.per_day': 'per day',
+
+  // Form labels
+  'form.distance': 'Distance (km)',
+
+  // Extra costs
+  'calculator.extra_costs.title': 'Extra costs',
+  'calculator.extra_costs.deposit_paid': 'You have paid a deposit',
+  'calculator.extra_costs.deposit_description':
+    'If you have paid a deposit you have the option to disable the mandatory reduction of the own risk which will cost',
+  'calculator.extra_costs.start_price': 'Has a start price',
+  'calculator.extra_costs.start_price_description':
+    "You have to pay a starting price when you don't have an abonnement or don't have trips. This cost",
+  'calculator.extra_costs.per_trip': 'per trip',
+  'calculator.extra_costs.read_more': 'To read more about this, follow this',
+  'calculator.trip.period': 'Period',
+
+  // Common
+  'common.link': 'link',
+} as const;
---
+++ libs/shared/util/src/lib/translations/nl.translations.ts
@@ -0,0 +1,24 @@
+export const nlTranslations = {
+  // Calculator
+  'calculator.title': 'mywheels calculator',
+
+  'calculator.price.per_day': 'per dag',
+
+  // Form labels
+  'form.distance': 'Afstand (km)',
+
+  // Extra costs
+  'calculator.extra_costs.title': 'Extra kosten',
+  'calculator.extra_costs.deposit_paid': 'Je hebt een borg betaald',
+  'calculator.extra_costs.deposit_description':
+    'Als je een borg hebt betaald heb je de optie om de verplichte verlaging van het eigen risico uit te schakelen wat kost',
+  'calculator.extra_costs.start_price': 'Heeft een startprijs',
+  'calculator.extra_costs.start_price_description':
+    'Je moet een startprijs betalen als je geen abonnement hebt of geen ritten hebt. Dit kost',
+  'calculator.extra_costs.per_trip': 'per rit',
+  'calculator.extra_costs.read_more': 'Om hier meer over te lezen, volg deze',
+  'calculator.trip.period': 'Periode',
+
+  // Common
+  'common.link': 'link',
+} as const;
