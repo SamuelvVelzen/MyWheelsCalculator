dd5384dce096c29bfca2ff7c48a272b894513779
Samuel <samuelvvelzen@icloud.com>
2025-06-22 10:26:58 +0200

    implement bar with language select above nav

--- apps/MyWheelsCalculator/src/app/main-layout/main-layout.component.ts
+++ apps/MyWheelsCalculator/src/app/main-layout/main-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DiscountButtonComponent } from '@mwc/ui';
+import { DiscountButtonComponent, LanguageSelectComponent } from '@mwc/ui';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css'],
   standalone: true,
-  imports: [RouterOutlet, DiscountButtonComponent],
+  imports: [RouterOutlet, DiscountButtonComponent, LanguageSelectComponent],
 })
 export class MainLayoutComponent {}
