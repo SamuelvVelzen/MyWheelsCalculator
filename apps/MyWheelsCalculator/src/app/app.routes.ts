7b36d34beb241e13b8052c3283295249359109e7
Samuel <samuelvvelzen@icloud.com>
2025-06-24 23:07:19 +0200

    add routes component

--- apps/MyWheelsCalculator/src/app/app.routes.ts
+++ apps/MyWheelsCalculator/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Route } from '@angular/router';
-import { CalculatorComponent } from '@mwc/calculator';
+import { CalculatorComponent, RoutesComponent } from '@mwc/calculator';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: MainLayoutComponent,
-    pathMatch: 'full',
-    children: [{ path: '', component: CalculatorComponent, pathMatch: 'full' }],
+    children: [
+      { path: 'routes', component: RoutesComponent, pathMatch: 'full' },
+      { path: '', component: CalculatorComponent },
+    ],
   },
   {
     path: '**',
