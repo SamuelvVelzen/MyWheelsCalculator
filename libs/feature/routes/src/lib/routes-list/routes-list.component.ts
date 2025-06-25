d87358b46b33c3d17216dcafaa8c8b77ca75bb07
Samuel <samuelvvelzen@icloud.com>
2025-06-25 08:55:49 +0200

    create list component

---
+++ libs/feature/routes/src/lib/routes-list/routes-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mwc-routes-list',
+  templateUrl: './routes-list.component.html',
+  styleUrls: ['./routes-list.component.css'],
+})
+export class RoutesListComponent {}
