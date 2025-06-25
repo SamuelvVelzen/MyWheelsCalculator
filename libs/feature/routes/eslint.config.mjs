1510409a83c08cf79142502a77d4707ce3435325
Samuel <samuelvvelzen@icloud.com>
2025-06-25 08:55:35 +0200

    update prefix

--- libs/feature/routes/eslint.config.mjs
+++ libs/feature/routes/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'lib',
+          prefix: 'mwc',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'lib',
+          prefix: 'mwc',
           style: 'kebab-case',
         },
       ],
