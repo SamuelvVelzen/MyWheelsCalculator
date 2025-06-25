03f05fd08e93a1328671bb531cc1e8abb10b1ff0
Samuel <samuelvvelzen@icloud.com>
2025-06-25 23:21:09 +0200

    export more components

--- libs/feature/calculator/src/index.ts
+++ libs/feature/calculator/src/index.ts
@@ -1,1 +1,14 @@
 export * from './lib/calculator.component';
+
+export * from './lib/_services/calculator-query-params.service';
+export * from './lib/_services/period.service';
+export * from './lib/_services/price.service';
+export * from './lib/_types/AbonnementOptionsEnum';
+export * from './lib/_types/AutoOptionsEnum';
+export * from './lib/_types/TripOptionsEnum';
+
+export * from './lib/abonnement-select/abonnement-select.component';
+export * from './lib/car-select/car-select.component';
+export * from './lib/extra-costs/extra-costs.component';
+
+export * from './lib/trip-select/trip-select.component';
