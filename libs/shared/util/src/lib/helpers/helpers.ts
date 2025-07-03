4df50bafc1e461c5bbc43cd8faeaae8b67a8155b
Samuel <samuelvvelzen@icloud.com>
2025-07-03 22:47:50 +0200

    add helper for isnot null

--- libs/shared/util/src/lib/helpers/helpers.ts
+++ libs/shared/util/src/lib/helpers/helpers.ts
@@ -1,3 +1,6 @@
 export class Helpers {
   static castFn = <T>(value: unknown) => value as T;
+
+  static isNotNullOrUndefined = <T>(value: T | null | undefined): value is T =>
+    value !== null && value !== undefined;
 }
