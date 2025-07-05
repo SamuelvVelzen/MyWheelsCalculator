7f5ea6fbc0ab04d6aaf376a75ba2183ad79f8d65
Samuel <samuelvvelzen@icloud.com>
2025-07-05 14:48:03 +0200

    move theme switching inside toggle

--- apps/MyWheelsCalculator/src/app/main-layout/main-layout.component.ts
+++ apps/MyWheelsCalculator/src/app/main-layout/main-layout.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ThemeService } from '@mwc/ui';
 import { MenuComponent } from '../ui/menu/menu.component';
 
 @Component({
@@ -11,13 +10,4 @@ import { MenuComponent } from '../ui/menu/menu.component';
   standalone: true,
   imports: [CommonModule, RouterOutlet, MenuComponent],
 })
-export class MainLayoutComponent {
-  private themeService = inject(ThemeService);
-
-  // Expose service signals for template
-  theme = this.themeService.theme;
-
-  toggleTheme() {
-    this.themeService.toggleTheme();
-  }
-}
+export class MainLayoutComponent {}
--- libs/shared/ui/src/lib/theme/theme-toggle/theme-toggle.component.ts
+++ libs/shared/ui/src/lib/theme/theme-toggle/theme-toggle.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'mwc-theme-toggle',
   templateUrl: './theme-toggle.component.html',
   styleUrls: ['./theme-toggle.component.css'],
 })
-export class ThemeToggleComponent {}
+export class ThemeToggleComponent {
+  private themeService = inject(ThemeService);
+
+  theme = this.themeService.theme;
+
+  buttonText = computed(() => {
+    const mode = this.theme() === 'dark' ? 'Light' : 'Dark';
+
+    return `Switch to ${mode} Mode`;
+  });
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
+}
