ad5b96699e04b69d8ce1b20a07302e49d023957c
Samuel <samuelvvelzen@icloud.com>
2025-07-05 14:46:00 +0200

    create theme toggle

---
+++ libs/shared/ui/src/lib/theme/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mwc-theme-toggle',
+  templateUrl: './theme-toggle.component.html',
+  styleUrls: ['./theme-toggle.component.css'],
+})
+export class ThemeToggleComponent {}
