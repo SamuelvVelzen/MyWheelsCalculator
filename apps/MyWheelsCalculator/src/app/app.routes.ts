339986a1e511d73c4590c13eb9ba7bfe1cd929b4
Samuel <samuelvvelzen@icloud.com>
2025-07-06 16:57:57 +0200

    add it to the routes

--- apps/MyWheelsCalculator/src/app/app.routes.ts
+++ apps/MyWheelsCalculator/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { CalculatorComponent } from '@mwc/calculator';
 import { hasRoutesGuard, RoutesComponent } from '@mwc/routes';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { HowItWorksPageComponent } from './pages/how-it-works-page/how-it-works-page.component';
 
 export const appRoutes: Route[] = [
   {
@@ -13,6 +14,10 @@ export const appRoutes: Route[] = [
         component: RoutesComponent,
         pathMatch: 'full',
         canDeactivate: [hasRoutesGuard],
+      },
+      {
+        path: 'how-it-works',
+        component: HowItWorksPageComponent,
       },
       { path: '', component: CalculatorComponent },
     ],
